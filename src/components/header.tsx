2980fd8ab3d7e1d7355571bffae418321a390edb
Anton Ishchyk <anton.ishchyk@gmail.com>
2025-02-27 14:56:07 +0100

    show the active page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
+const navLinks = [
+  { path: "/", label: "Home" },
+  { path: "/about", label: "About Me" },
+  { path: "/projects", label: "Projects" },
+  { path: "/contact", label: "Contact" }
+];
 
 const Header: React.FC = () => {
+  const location = useLocation();
+
   return (
-    <header className="sticky top-0 bg-[#333333] text-white shadow-lg z-10">
+    <header className="sticky top-0 bg-[#333] text-white shadow-lg z-10">
       <div className="flex justify-between items-center p-4">
-        <div className="logo">
-          <Link to="/" className="text-xl font-bold">My Portfolio</Link>
-        </div>
+        <Link to="/" className="text-xl font-bold">My Portfolio</Link>
         <nav>
           <ul className="flex space-x-6">
-            <li>
-              <Link to="/" className="hover:text-[#CFC7A3] transition duration-300">Home</Link>
-            </li>
-            <li>
-              <Link to="/about" className="hover:text-[#CFC7A3] transition duration-300">About Me</Link>
-            </li>
-            <li>
-              <Link to="/projects" className="hover:text-[#CFC7A3] transition duration-300">Projects</Link>
-            </li>
-            <li>
-              <Link to="/contact" className="hover:text-[#CFC7A3] transition duration-300">Contact</Link>
-            </li>
+            {navLinks.map(({ path, label }) => (
+              <li key={path} className="relative">
+                <Link 
+                  to={path} 
+                  className={`hover:text-[#CFC7A3] transition duration-300 
+                  ${location.pathname === path ? "text-[#CFC7A3] font-semibold" : ""}`}>
+                  {label}
+                </Link>
+                {location.pathname === path && (
+                  <div className="absolute left-0 bottom--1 w-full h-[3px] bg-[#CFC7A3] rounded-full"></div>
+                )}
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
