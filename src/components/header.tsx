5b1dc7998f1051f27c2d2ef18c11cbb5cb02b8e1
Anton Ishchyk <anton.ishchyk@gmail.com>
2025-02-27 15:08:09 +0100

    dry

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,19 +17,21 @@ const Header: React.FC = () => {
         <Link to="/" className="text-xl font-bold">My Portfolio</Link>
         <nav>
           <ul className="flex space-x-6">
-            {navLinks.map(({ path, label }) => (
-              <li key={path} className="relative">
-                <Link 
-                  to={path} 
-                  className={`hover:text-[#CFC7A3] transition duration-300 
-                  ${location.pathname === path ? "text-[#CFC7A3] font-semibold" : ""}`}>
-                  {label}
-                </Link>
-                {location.pathname === path && (
-                  <div className="absolute left-0 bottom--1 w-full h-[3px] bg-[#CFC7A3] rounded-full"></div>
-                )}
-              </li>
-            ))}
+          {navLinks.map(({ path, label }) => {
+              const isActive = location.pathname === path;
+              return (
+                <li key={path} className="relative">
+                  <Link
+                    to={path}
+                    className={`hover:text-[#CFC7A3] transition duration-300 ${isActive ? "text-[#CFC7A3] font-semibold" : ""}`}>
+                    {label}
+                  </Link>
+                  {isActive && (
+                    <div className="absolute left-0 bottom-[-1px] w-full h-[3px] bg-[#CFC7A3] rounded-full"></div>
+                  )}
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </div>
