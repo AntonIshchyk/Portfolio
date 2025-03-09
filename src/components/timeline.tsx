f93ed576a7c9c1018f4c52c94024663911f0a055
Anton Ishchyk <anton.ishchyk@gmail.com>
2025-03-09 13:32:28 +0100

    center the data

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,18 +1,15 @@
-import {Education, Work} from "../data/timeline"
+import { Education, Work } from "../data/timeline";
 
 const TimelineItem: React.FC<Education | Work> = (props) => {
   return (
-    <div className="group relative py-6 pl-8 sm:pl-32">
-      <div className="mb-1 flex flex-col items-start before:absolute before:left-2 before:h-full before:-translate-x-1/2 before:translate-y-3 before:self-start before:bg-slate-300 before:px-px after:absolute after:left-2 after:box-content after:h-2 after:w-2 after:-translate-x-1/2 after:translate-y-1.5 after:rounded-full after:border-4 after:border-slate-50 after:bg-indigo-600 group-last:before:hidden sm:flex-row sm:before:left-0 sm:before:ml-[6.5rem] sm:after:left-0 sm:after:ml-[6.5rem]">
+    <div className="group relative py-6 pl-8 sm:pl-32 flex justify-center">
+      <div className="w-full max-w-2xl mb-1 flex flex-col items-start before:absolute before:left-2 before:h-full before:-translate-x-1/2 before:translate-y-3 before:self-start before:bg-slate-300 before:px-px after:absolute after:left-2 after:box-content after:h-2 after:w-2 after:-translate-x-1/2 after:translate-y-1.5 after:rounded-full after:border-4 after:border-slate-50 after:bg-indigo-600 group-last:before:hidden sm:flex-row sm:before:left-0 sm:before:ml-[6.5rem] sm:after:left-0 sm:after:ml-[6.5rem]">
         <time className="left-0 mb-3 inline-flex h-6 w-20 translate-y-0.5 items-center justify-center rounded-full bg-emerald-100 text-xs font-semibold uppercase text-emerald-600 sm:absolute sm:mb-0">
           {props.date}
         </time>
         {"university" in props ? (
           <div>
-            <a href={props.link} 
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-xl font-bold">
+            <a href={props.link} target="_blank" rel="noopener noreferrer" className="text-xl font-bold">
               {props.university}
             </a>
             <div>{props.degree} in {props.field} | {props.form}</div>
@@ -20,10 +17,7 @@ const TimelineItem: React.FC<Education | Work> = (props) => {
         ) : (
           <div>
             <div className="text-xl font-bold">
-              <a 
-                href={props.link} 
-                target="_blank"
-                rel="noopener noreferrer">
+              <a href={props.link} target="_blank" rel="noopener noreferrer">
                 {props.title} at {props.company}
               </a>
             </div>
@@ -42,23 +36,23 @@ interface TimelineProps {
 
 const Timeline: React.FC<TimelineProps> = ({ education, work }) => {
   return (
-    <div>
+    <div className="w-full flex flex-col items-center">
       {education && (
-        <>
-          <h2 className="text-2xl font-bold text-gray-800">Education</h2>
+        <div className="w-full max-w-2xl">
+          <h2 className="text-2xl font-bold">Education</h2>
           {education.map((item, idx) => (
             <TimelineItem key={idx} {...item} />
           ))}
-        </>
+        </div>
       )}
       
       {work && (
-        <>
-          <h2 className="text-2xl font-bold text-gray-800 mt-4">Work Experience</h2>
+        <div className="w-full max-w-2xl mt-4">
+          <h2 className="text-2xl font-bold">Work Experience</h2>
           {work.map((item, idx) => (
             <TimelineItem key={idx} {...item} />
           ))}
-        </>
+        </div>
       )}
     </div>
   );
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,36 +3,36 @@ import { TfiDownload } from "react-icons/tfi";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import CV from "../assets/about/CV Software Developer Anton Ishchyk.pdf";
 import Timeline from "../components/timeline";
-import {education, work} from "../data/timeline"
-
+import { education, work } from "../data/timeline";
 
 const About: React.FC = () => {
-    return (
-      <>
-        <a
-          href={CV}
-          download="CV Software Developer Anton Ishchyk.pdf"
-          className="bg-gradient-to-r from-blue-500 to-teal-500 text-white py-2 px-6 rounded-full inline-flex items-center shadow-lg hover:scale-105 transition-all">
-          <TfiDownload className="mr-2 text-xl" /> Download Resume
+  return (
+    <div className="w-full flex flex-col items-center">
+      <a
+        href={CV}
+        download="CV Software Developer Anton Ishchyk.pdf"
+        className="bg-gradient-to-r from-blue-500 to-teal-500 text-white py-2 px-6 rounded-full inline-flex items-center shadow-lg hover:scale-105 transition-all">
+        <TfiDownload className="mr-2 text-xl" /> Download Resume
+      </a>
+
+      <div className="w-full max-w-2xl text-left">
+        <Timeline education={education} />
+        <Timeline work={work} />
+      </div>
+
+      <div className="mt-6 flex space-x-4">
+        <a href="https://github.com/AntonIshchyk" target="_blank" rel="noopener noreferrer" className="text-[#333] hover:text-black text-3xl">
+          <FaGithub />
         </a>
+        <a href="https://www.linkedin.com/in/anton-ishchyk/" target="_blank" rel="noopener noreferrer" className="text-[#333] hover:text-blue-600 text-3xl">
+          <FaLinkedin />
+        </a>
+        <a href="mailto:anton.ishchyk@gmail.com" className="text-[#333] hover:text-red-500 text-3xl">
+          <FaEnvelope />
+        </a>
+      </div>
+    </div>
+  );
+};
 
-        <Timeline education={education}/>
-
-        <Timeline work={work}/>
-
-        <div className="mt-6 flex space-x-4">
-          <a href="https://github.com/AntonIshchyk" target="_blank" rel="noopener noreferrer" className="text-[#333] hover:text-black text-3xl">
-            <FaGithub />
-          </a>
-          <a href="https://www.linkedin.com/in/anton-ishchyk/" target="_blank" rel="noopener noreferrer" className="text-[#333] hover:text-blue-600 text-3xl">
-            <FaLinkedin />
-          </a>
-          <a href="mailto:anton.ishchyk@gmail.com" className="text-[#333] hover:text-red-500 text-3xl">
-            <FaEnvelope />
-          </a>
-        </div>
-      </>
-    );
-  }
-  
-export default About;    
+export default About;
