74faad4af80d2aaf0795f709517b816827fc0781
Anton Ishchyk <anton.ishchyk@gmail.com>
2025-03-12 21:03:34 +0100

    remove unused

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { TfiDownload } from "react-icons/tfi";
-import { FaGithub, FaLinkedin, FaEnvelope, FaTelegram  } from "react-icons/fa";
 import CV from "../assets/about/CV Software Developer Anton Ishchyk.pdf";
 import Timeline from "../components/timeline";
 import { education, work } from "../data/timeline";
@@ -8,35 +7,6 @@ import { education, work } from "../data/timeline";
 const About: React.FC = () => {
   return (
     <div className="w-full flex flex-col items-center px-6 py-8">
-      <div className="flex space-x-6 mb-12">
-        <a
-          href="https://www.linkedin.com/in/anton-ishchyk/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-[#333] hover:text-blue-600 text-4xl transition-all duration-200">
-          <FaLinkedin />
-        </a>
-        <a
-          href="https://t.me/Anton_Ishchyk"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-[#333] hover:text-[#0088cc] text-4xl transition-all duration-200">
-          <FaTelegram />
-        </a>
-        <a
-          href="mailto:anton.ishchyk@gmail.com"
-          className="text-[#333] hover:text-red-500 text-4xl transition-all duration-200">
-          <FaEnvelope />
-        </a>
-        <a
-          href="https://github.com/AntonIshchyk"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-[#333] hover:text-black text-4xl transition-all duration-200">
-          <FaGithub />
-        </a>
-      </div>
-
       <div className="mb-16">
         <a
           href={CV}
