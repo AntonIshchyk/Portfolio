1307524cc04cdfe6bd87e1f1253c60309e4c5bb3
Anton Ishchyk <anton.ishchyk@gmail.com>
2025-03-21 16:43:07 +0100

    skills wip

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,6 +4,13 @@ import CV from "../assets/about/CV Software Developer Anton Ishchyk.pdf";
 import voedselbank from "../assets/about/voedselbank-maassluis.png";
 import exc from "../assets/about/exc.png";
 import delphis from "../assets/about/delphis.png";
+import { FaReact, FaGitAlt, FaDocker, FaDatabase } from "react-icons/fa";
+import { IoLogoJavascript, IoLogoPython } from "react-icons/io";
+import { SiDotnet } from "react-icons/si";
+
+import { FaGithub,} from "react-icons/fa";
+import { SiSqlite, SiTypescript, SiSwagger,} from "react-icons/si";
+import { DiScrum } from "react-icons/di";
 import Timeline from "../components/timeline";
 import { education, work } from "../data/timeline";
 
@@ -18,6 +25,46 @@ const About: React.FC = () => {
       </a>
       <Timeline education={education} />
       <Timeline work={work} />
+      
+      <div className="w-full max-w-2xl flex flex-col mb-6">
+        <h2 className="text-2xl font-bold">Skills</h2>
+        <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+          <div>
+            <h4 className="font-semibold">Programming Languages</h4>
+            <div className="flex gap-2 flex-wrap mt-2">
+              <SiDotnet size={40} className="hover:text-[#512bd4] transition-colors duration-300" />
+              <SiTypescript size={40} className="hover:text-[#3178C6] transition-colors duration-300 ml-2" />
+              <IoLogoPython size={40} className="hover:text-[#306998] transition-colors duration-300" />
+            </div>
+          </div>
+
+          <div>
+            <h4 className="font-semibold">Frameworks/Technologies</h4>
+            <div className="flex gap-2 flex-wrap">
+              <FaReact size={40} className="hover:text-[#61DBFB] transition-colors duration-300" />
+              <FaDatabase size={40} className="hover:text-[#00758F] transition-colors duration-300" />
+            </div>
+          </div>
+
+          <div>
+            <h4 className="font-semibold">Development Practices</h4>
+            <div className="flex gap-2 flex-wrap">
+              <FaGitAlt size={40} className="hover:text-[#f04141] transition-colors duration-300" />
+              <DiScrum size={40} className="hover:text-[#e10098] transition-colors duration-300"/>
+            </div>
+          </div>
+
+          <div>
+            <h4 className="font-semibold">Languages</h4>
+            <div className="flex gap-2 flex-wrap">
+              English
+              Dutch
+              Ukrainian
+              Russian
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div className="w-full max-w-2xl flex flex-col items-start">
         <h2 className="text-2xl font-bold">Other Activities</h2>
